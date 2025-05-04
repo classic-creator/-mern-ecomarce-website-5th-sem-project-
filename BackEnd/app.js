b6801654b22acc6299da0242159338d8d7212cb2
classic-creator <himangshukhound322@gmail.com>
2025-05-04 23:04:15 +0530

    aasdad

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -20,6 +20,7 @@ app.use(cors({
     credentials: true
   }));
   
+
 app.use(express.json())
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({extended:true}))
--- BackEnd/utils/jwtToken.js
+++ BackEnd/utils/jwtToken.js
@@ -4,11 +4,13 @@ const sendToken= (user,statusCode,res)=>{
     const token= user.getJWTToken();
 
     const options={
-        expire:new Date(
+        expires:new Date(
 
             Date.now()+process.env.COOKIE_EXPIRE*24*60*60*1000
         ),
         httpOnly:true,
+        secure: true, 
+        sameSite: "None",
     };
 
     res.status(statusCode).cookie("token",token,options).json({
